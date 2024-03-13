6537cedb0e1dd8a063be3634e7cdf6909b176f64
Carlos Eduardo <carloseduardo15000@gmail.com>
2024-03-13 18:41:32 -0300

    renomear

--- desafios/src/InversorDePalavras05.java
+++ desafios/src/InversorDePalavras05.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class InversorDePalavras {
+public class InversorDePalavras05 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
