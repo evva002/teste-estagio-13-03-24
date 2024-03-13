c78f7a3b84971bef3865126df80387ed2f3aa83e
Carlos Eduardo <carloseduardo15000@gmail.com>
2024-03-13 18:39:58 -0300

    todos completos

---
+++ desafios/src/InversorDePalavras.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class InversorDePalavras {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String palavraInvertida = "";
+        
+        System.out.println("Digite uma palavra para ser invertida: ");
+        String palavraUsuario = scanner.nextLine();
+
+        for ( int i = palavraUsuario.length() -1 ; i >= 0 ; i--){
+            palavraInvertida = palavraInvertida + Character.toString(palavraUsuario.charAt(i));
+        }
+        System.out.println(palavraInvertida);
+
+        scanner.close();
+    }
+}
